c25022cad905bb7921d2eedb34fe23ffe3c7f413
donlabin <donlabin@users.noreply.github.com>
2019-07-17 09:28:39 +0800

    Update Syntax.py
    add Data Analysis Pipeline description

--- Syntax.py
+++ Syntax.py
@@ -1,2 +1,18 @@
-Print("hello world")
-Print("What the fxck!")
+### Data Analysis Pipeline ###
+
+# Get Data
+
+# Clean Data
+
+# EDA(Explorary Data Analysis)
+
+# Plot Data
+
+# Define the problem
+
+# Train the model
+
+# Validation the model
+
+# Deploy the application
+
