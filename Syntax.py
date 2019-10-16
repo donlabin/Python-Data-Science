7953e94e6f77276e13f45969fb4dfe86c75df81b
donlabin <donlabin@users.noreply.github.com>
2019-10-16 17:01:54 +0800

    Update Syntax.py
    新增一些常用語法

--- Syntax.py
+++ Syntax.py
@@ -1,8 +1,13 @@
 ### Data Analysis Pipeline ###
 
 # Get Data
+reference:https://www.datacamp.com/community/tutorials/finance-python-trading
+1.透過pandas_datareader抓線上資料
+2.read_csv()
 
-# Preprocess & Clean Data & Save Data to DB
+# Explore & Preprocess & Clean Data & Save Data to DB
+head(),tail(),describe()
+to_csv()
 
 # EDA(Explorary Data Analysis)
 
