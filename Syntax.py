2b930df580dbbfe7d2ab0b90b0cce659f0c7df6f
donlabin <donlabin@users.noreply.github.com>
2019-10-17 23:21:58 +0800

    Update Syntax.py
    新增探索資料的語法

--- Syntax.py
+++ Syntax.py
@@ -7,11 +7,33 @@ reference:https://www.datacamp.com/community/tutorials/finance-python-trading
 
 # Explore & Preprocess & Clean Data & Save Data to DB
 head(),tail(),describe()
+df.index,df.columns # df means Dataframe
+ts = aapl['Close'][-10:] # Subset Dataframe
+loc() and iloc() # you use the former for label-based indexing and the latter for positional indexing. 
+                 # the most idiomatic way to do things with Pandas
+EX:
+print(aapl.loc[pd.Timestamp('2006-11-01'):pd.Timestamp('2006-12-31')].head())
+print(aapl.loc['2007'].head(1))
+print(aapl.iloc[22:43])
+print(aapl.iloc[[22,43], [0, 3]])
+
+sample(),resample(),asfreq()
+EX:
+sample = aapl.sample(20)
+monthly_aapl = aapl.resample('M').mean()
+aapl.asfreq("M", method="bfill")
+
+aapl['diff'] = aapl.Open - aapl.Close # Add a column `diff` to `aapl` 
+del aapl['diff'] # Delete the new `diff` column
+type(ts)
 to_csv()
 
 # EDA(Explorary Data Analysis)
 
-# Plot Data
+# Plot Data(visualize time series data)
+import matplotlib.pyplot as plt
+aapl['Close'].plot(grid=True)
+plt.show()
 
 # Define the problem
 
